ae8e855dc096626fc69d603ba8287c38950d32d3
reng <reng412@gmail.com>
2020-12-13 11:45:54 -0800

    Clean up comments in public.js

--- public/js/public.js
+++ public/js/public.js
@@ -1,19 +1,12 @@
 $(document).on("ready", () => {
-    // Create POST request to database with new url entry and shortcode
+    // Create POST request to database with new url entry and random shortcode
     $("#urlFormRandom").on("submit", e => {
         e.preventDefault();
-        let urlData = $(this).data
+        // let urlData = $(this).data
         // let shortUrls = urlData.map(url => {
         //     url.short
         // })
-        console.log(urlData);
-        // If a user's shortcode is available, add to the database
-        // let newEntry = {
-        //     url: $("#longUrl").val(),
-        //     short: $("#shortcode").val()
-        // };
-        // If a user's shortcode is not available, return error message
-
+        // console.log(urlData);
         // If a user doesn't specify a shortcode, create a random shortcode
         let newShortcode = "";
         function generateShortcode() {
@@ -25,61 +18,36 @@ $(document).on("ready", () => {
             console.log(newShortcode);
         }
         generateShortcode();
-        let newEntry = {
+        let newEntryRandom = {
             url: $("#longUrlRandom").val(),
             short: newShortcode
         };
 
-        console.log(newEntry);
-        $("#longUrlRandom").val("");
-        $("#shortcodeRandom").val("");
+        console.log(newEntryRandom);
 
         $.ajax("/newUrl", {
             type: "POST",
-            data: newEntry
+            data: newEntryRandom
         }).then(
             () => {
                 location.reload();
             }
         );
     });
+
+    // Create POST request to database with new url entry and user generated shortcode
     $("#urlFormUser").on("submit", e => {
         e.preventDefault();
-        // let urlData = $(this).data
-        // let shortUrls = urlData.map(url => {
-        //     url.short
-        // })
-        // console.log(urlData);
-        // If a user's shortcode is available, add to the database
-        let newEntry = {
+        let newEntryUser = {
             url: $("#longUrlUser").val(),
             short: $("#shortcodeUser").val()
         };
-        // If a user's shortcode is not available, return error message
+        console.log(newEntryUser);
 
-        // If a user doesn't specify a shortcode, create a random shortcode
-        // let newShortcode = "";
-        // function generateShortcode() {
-        //     console.log("Generating code")
-        //     const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        //     for (let i = 0; i < 6; i++) {
-        //         newShortcode += characters[Math.floor(Math.random() * 62)]
-        //     }
-        //     console.log(newShortcode);
-        // }
-        // generateShortcode();
-        // let newEntry = {
-        //     url: $("#longUrl").val(),
-        //     short: newShortcode
-        // };
-
-        console.log(newEntry);
-        $("#longUrl").val("");
-        $("#shortcode").val("");
 
         $.ajax("/newUrl", {
             type: "POST",
-            data: newEntry
+            data: newEntryUser
         }).then(
             () => {
                 location.reload();
